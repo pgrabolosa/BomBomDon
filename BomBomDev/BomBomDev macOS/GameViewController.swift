ca1b772d23c9975a2940e22f892e5665f20f6a02
Pierre Grabolosa <pgrabolosa@gmail.com>
2020-11-13 21:26:14 +0100

    Empty project

---
+++ BomBomDev/BomBomDev macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  BomBomDev macOS
+//
+//  Created by Pierre Grabolosa on 13/11/2020.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
