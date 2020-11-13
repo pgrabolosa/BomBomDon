5c9a862edcb16db18bbcc0161bf2fdf0c66937c1
Pierre Grabolosa <pgrabolosa@gmail.com>
2020-11-13 22:37:41 +0100

    base

--- BomBomDev/BomBomDev Shared/Experiments/Boutique/ExpBoutiqueScene.swift
+++ BomBomDev/BomBomDev Shared/Experiments/Boutique/ExpBoutiqueScene.swift
@@ -6,15 +6,42 @@
 //
 
 import SpriteKit
+import GameplayKit
 
 class ExpBoutiqueScene : SKScene {
+    
+    var drawerSprite: SKShapeNode!
     
     class func newScene() -> ExpBoutiqueScene {
         guard let scene = SKScene(fileNamed: "ExpBoutiqueScene") as? ExpBoutiqueScene else {
             fatalError("Failed to find ExpBoutiqueScene")
         }
-        scene.scaleMode = .aspectFill
+        scene.scaleMode = .aspectFit
         return scene
+    }
+    
+    override func didMove(to view: SKView) {
+        // initial setup
+        
+        drawerSprite = SKShapeNode(rectOf: CGSize(width: 200, height: self.frame.height - 50))
+        drawerSprite.fillColor = .clear
+        drawerSprite.strokeColor = .black
+        
+        
+        
+        addChild(drawerSprite)
+        
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
+            
+            let item = SKShapeNode(rectOf: CGSize(width: 50, height: 50))
+            item.fillColor = .red
+            
+            self.drawerSprite.addChild(item)
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
     }
     
 }
