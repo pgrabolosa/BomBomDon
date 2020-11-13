d4d37f9637abb64fcb5d8ab690fa76ed176392d0
Pierre Grabolosa <pgrabolosa@gmail.com>
2020-11-13 22:48:12 +0100

    adding items in the drawer

--- BomBomDev/BomBomDev Shared/Experiments/Boutique/ExpBoutiqueScene.swift
+++ BomBomDev/BomBomDev Shared/Experiments/Boutique/ExpBoutiqueScene.swift
@@ -8,7 +8,25 @@
 import SpriteKit
 import GameplayKit
 
+class Boutique {
+    
+    var items: [String] = []
+    
+    func append(_ item: String) {
+        items.append(item)
+        NotificationCenter.default.post(name: .BoutiqueNewItem, object: self, userInfo: ["item": item])
+    }
+    
+}
+
+extension Notification.Name {
+    static let BoutiqueNewItem = Notification.Name("BoutiqueNewItem")
+}
+
+
 class ExpBoutiqueScene : SKScene {
+    
+    let boutique = Boutique()
     
     var drawerSprite: SKShapeNode!
     
@@ -27,16 +45,24 @@ class ExpBoutiqueScene : SKScene {
         drawerSprite.fillColor = .clear
         drawerSprite.strokeColor = .black
         
-        
-        
         addChild(drawerSprite)
         
         Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
-            
+            self.boutique.append("Test \(1 + self.boutique.items.count)")
+        }
+        
+        NotificationCenter.default.addObserver(forName: .BoutiqueNewItem, object: nil, queue: .main) { (notification) in
             let item = SKShapeNode(rectOf: CGSize(width: 50, height: 50))
             item.fillColor = .red
-            
             self.drawerSprite.addChild(item)
+            
+            // reorder items
+            var position = CGPoint.zero
+            position.y = -CGFloat(self.boutique.items.count * 50 + (self.boutique.items.count-1) * 20)/2
+            for child in self.drawerSprite.children {
+                child.position = position
+                position.y += 50 + 20
+            }
         }
     }
     
