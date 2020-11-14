3e3074880e51265af6eedc594ea3a6f12da3a075
Pierre Grabolosa <pgrabolosa@gmail.com>
2020-11-14 22:30:45 +0100

    some commenting and works on iOS

--- BomBomDev/BomBomDev iOS/GameViewController.swift
+++ BomBomDev/BomBomDev iOS/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
+        let scene = GameScene.newScene()
 
         // Present the scene
         let skView = self.view as! SKView
